4db5cfcc055be2aa572a4b273e2a5efd7400db55
Myzsoff <hervanedenis973@gmail.com>
2025-05-30 04:59:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
+        buyer: ['519571897743048709','1231402589275291669'], // Buyer
         couleur: '#6495ED', // Couleur HEX
         footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
-        maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
+        maxServer: '2', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
